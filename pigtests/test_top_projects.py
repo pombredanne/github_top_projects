37198f2d11541af26fdf7d965e8a4d45198300e9
Jeremy Karn <jkarn@mortardata.com>
2013-11-14 15:00:31 -0800

    Simplify test for demo

--- pigtests/test_top_projects.py
+++ pigtests/test_top_projects.py
@@ -1,4 +1,3 @@
-#from pigtest import PigTestCase, main
 from testlib import PigTestCase
 from unittest import main
 
@@ -9,14 +8,14 @@ class FilterTest(PigTestCase):
     def test__results__sorted_correctly(self):
         self.stubAlias('top_repos', [
             ['2013-02', 'repo_1', 3],
-            ['2013-01', 'repo_2', 2],
+            ['2013-02', 'repo_2', 2],
             ['2013-02', 'repo_3', 7]
         ])
 
         self.assertAliasEquals('results', [
-            ('2013-01', 'repo_2', 2),
             ('2013-02', 'repo_3', 7),
             ('2013-02', 'repo_1', 3),
+            ('2013-02', 'repo_2', 2),
         ])
 
 if __name__ == '__main__':
